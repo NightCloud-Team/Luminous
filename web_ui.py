fc0bb4fd3f7564c48304433bda39e66d7c93b257
xueruhao <1194943917@qq.com>
2024-07-04 14:25:21 +0800

    规范代码并初步设计UI

--- web_ui.py
+++ web_ui.py
@@ -2,10 +2,25 @@ import flask
 
 app = flask.Flask(__name__)
 
+
 @app.route('/')
 def index():
     return flask.render_template('index.html')
-@app.route('/system')
-def system():
-    return flask.render_template('system.html')
+
+
+@app.route('/software')
+def software():
+    return flask.render_template('software.html')
+
+
+@app.route('/SpecialThanks')
+def SpecialThanks():
+    return flask.render_template('SpecialThanks.html')
+
+
+@app.route('/website')
+def website():
+    return flask.render_template('website.html')
+
+
 app.run(host='127.0.0.1', port=8000)
