ca86d0e5f4846484b6b6817c215adcc5742ae9ab
lijiaxuan <lijiaxuan1811@163.com>
2024-11-08 19:33:36 -0500

    添加关于菜单

--- luminous/app/src/main/java/com/luminous/luminous/MainActivity.java
+++ luminous/app/src/main/java/com/luminous/luminous/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_settings)
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_settings, R.id.nav_about)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
@@ -52,9 +52,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.main, menu);
-        //return true;
+        /* Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;*/
         return false;
     }
 
---
+++ luminous/app/src/main/java/com/luminous/luminous/ui/about/AboutViewModel.java
@@ -0,0 +1,7 @@
+package com.luminous.luminous.ui.about;
+
+import androidx.lifecycle.ViewModel;
+
+public class AboutViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
